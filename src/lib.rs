54795463af19db55d8c36dd950328097a0152cb2
Radical <benjaminjohnsen.bgj@gmail.com>
2022-05-27 19:57:05 +0200

    Fix multiple issues

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,27 @@ use cpython::{PyResult, Python, py_module_initializer, py_fn};
 // N.B: names: "rust2py" must be the name of the `.so` or `.pyd` file
 py_module_initializer!(miscLib, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(py, "str_replacer", py_fn!(py, str_replacer(initial_string: String, ch: String, replacing_character: String, occurence: i64)))?;
+    m.add(py, "str_replacer", py_fn!(py, str_replacer(initial_string: String, ch: String, replacing_character: String, occurrence: i64)))?;
     Ok(())
 });
 
-pub fn str_replacer(_py: Python, initial_string: String, ch: String, replacing_character: String, occurence: i64) -> PyResult<String> {
+pub fn str_replacer(_py: Python, initial_string: String, ch: String, replacing_character: String, occurrence: i64) -> PyResult<String> {
     let mut result: String = "".to_string();
     let mut count = 0;
     for c in initial_string.chars() {
-        if c == ch.chars().nth(0).unwrap() && count != occurence {
-            result.push(replacing_character.chars().nth(0).unwrap());
-            count += 1;
+        count += 1;
+        if c == ch.chars().nth(0).unwrap() {
+            if count == occurrence {
+                result.push(replacing_character.chars().nth(0).unwrap());
+                count = 0;
+            } else {
+                result.push(c);
+            }
         } else {
             result.push(c);
+            if count >= occurrence {
+                count = 0;
+            }
         }
     }
     
