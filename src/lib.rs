e3ec2c98bb9ff397aa92373f1b293cef6878a0ed
Radical <benjaminjohnsen.bgj@gmail.com>
2022-05-28 16:44:28 +0200

    Miscommunication

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 use cpython::{PyResult, Python, py_module_initializer, py_fn};
 
-// add bindings to the generated python module
-// N.B: names: "rust2py" must be the name of the `.so` or `.pyd` file
 py_module_initializer!(miscLib, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
     m.add(py, "str_replacer", py_fn!(py, str_replacer(initial_string: String, ch: String, replacing_character: String, occurrence: i64)))?;
@@ -12,8 +10,8 @@ pub fn str_replacer(_py: Python, initial_string: String, ch: String, replacing_c
     let mut result: String = "".to_string();
     let mut count = 0;
     for c in initial_string.chars() {
-        count += 1;
         if c == ch.chars().nth(0).unwrap() {
+            count += 1;
             if count == occurrence {
                 result.push(replacing_character.chars().nth(0).unwrap());
                 count = 0;
@@ -22,9 +20,6 @@ pub fn str_replacer(_py: Python, initial_string: String, ch: String, replacing_c
             }
         } else {
             result.push(c);
-            if count >= occurrence {
-                count = 0;
-            }
         }
     }
     
